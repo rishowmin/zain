120a00c36df5db80c5509b0e581db2e67ae94c44
Raisul Showmin <rishowmin.seu38@gmail.com>
2022-02-28 23:20:07 +0600

    Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,29 @@
+let navToggle = document.querySelector(".nav_toggle");
+let navWrapper = document.querySelector(".nav_wrapper");
 
- const navToggler = document.querySelector(".nav-toggler");
- navToggler.addEventListener("click", navToggle);
+navToggle.addEventListener("click", function () {
+  if (navWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "menu");
+    navWrapper.classList.remove("active");
+  } else {
+    navWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close menu");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
 
- function navToggle() {
-    navToggler.classList.toggle("active");
-    const nav = document.querySelector(".nav");
-    nav.classList.toggle("open");
-    if(nav.classList.contains("open")){
-    	nav.style.maxHeight = nav.scrollHeight + "px";
-    }
-    else{
-    	nav.removeAttribute("style");
-    }
- } 
+let cartToggle = document.querySelector(".cart_toggle");
+let cartWrapper = document.querySelector(".cart_wrapper");
+
+cartToggle.addEventListener("click", function () {
+  if (cartWrapper.classList.contains("active")) {
+    this.setAttribute("aria-expanded", "false");
+    this.setAttribute("aria-label", "cart");
+    cartWrapper.classList.remove("active");
+  } else {
+    cartWrapper.classList.add("active");
+    this.setAttribute("aria-label", "close cart");
+    this.setAttribute("aria-expanded", "true");
+  }
+});
